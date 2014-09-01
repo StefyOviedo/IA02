9eab7f6fb2c4067c12c7ab1e0c7d05d62122fc51
StefyOviedo <spoviedo22@gmail.com>
2014-09-01 15:11:44 -0500

    Update Trabajo05.js

--- Trabajo05.js
+++ Trabajo05.js
@@ -9,19 +9,22 @@ function numeros(){
 	 console.log("RESULTADOS");
 
 	 for (var i = 0 ; i < 6; i++) {
-	 	var par = pares(i);
-	 	var primo = primos(i);
+	 	var par = pares(opr[i]);
+	 	var primo = primos(opr[i]);
 
 	 	if (par && primo) {
 	 		console.log("El Numero " + opr[i] + " es un numero par y  primo, ");
 	 	}else
 	 	{
 	 		if (par && !primo) {
-	 			console.log("El Numero " + opr[i] + " es par, ");
+	 			console.log("El Numero " + opr[i] + " es un numero par, ");
 	 		}else
 	 		{
-	 			console.log("El Numero " + opr[i] + " es primo, ");
+	 			if (!par && primo){
+	 			console.log("El Numero " + opr[i] + " es un numero primo, ");
+}
 	 		}
+
 	 	}
 	 }
 }
@@ -38,7 +41,7 @@ function pares(num){
 
 function primos(num)
 {
-	if (num>2) {
+	if (num>=2) {
 		var cont = 2;
 		var resul = true;
 
